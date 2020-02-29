bfc7724be30b0250af2af95f08e7761661d6b9a0
Your Name <you@example.com>
2020-02-29 15:37:26 +0300

    renames

--- server/core.js
+++ server/core.js
@@ -1,6 +1,46 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+let fs = require('fs');
+const path = require('path');
+const promisify = require('util').promisify;
+const readdirp = promisify(fs.readdir);
+const statp = promisify(fs.stat);
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const args = process.argv.slice(2).reduce((acc, arg) => {
+    let [key, value] = arg.split('=');
+    acc[key] = value;
+    return acc;
+}, {});
+
+let dirPath = __dirname + args['directory'];
+
+async function scan(directoryName, results = []) {
+    let files = await readdirp(directoryName);
+    for (let f of files) {
+        let fullPath = path.join(directoryName, f);
+        let stat = await statp(fullPath);
+
+        if (stat.isDirectory()) {
+            results.push(fullPath);
+            await scan(fullPath, results);
+        }
+    }
+    return results;
+}
+
+(async () => {
+    if (fs.existsSync(dirPath) && fs.lstatSync(dirPath).isDirectory()) {
+        let results = await scan(dirPath, []);
+        let dateRegularExpression = new RegExp(/(.*)(\d{2}).(\d{2}).(\d{4})/);
+
+        results = results.filter(path => {
+            console.log('path:', path);
+            return dateRegularExpression.test(path)
+        });
+
+        results.forEach(path => {
+            let newName = path.replace(dateRegularExpression, "$1$4.$3.$2");
+            fs.rename(path, newName, function (err) {
+                if (err) console.log('ERROR: ' + err);
+            });
+        });
+    }
+})();
