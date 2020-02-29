bd4243e2d2ee4933414f2d21c1b0e972f8a9b03d
Your Name <you@example.com>
2020-02-29 16:45:14 +0300

    update core

--- server/core.js
+++ server/core.js
@@ -29,15 +29,15 @@ async function scan(directoryName, results = []) {
 (async () => {
     if (fs.existsSync(dirPath) && fs.lstatSync(dirPath).isDirectory()) {
         let results = await scan(dirPath, []);
-        let dateRegularExpression = new RegExp(/(.*)(\d{2}).(\d{2}).(\d{4})/);
+        let dateRegularExpression = new RegExp(/(.*)(\d{2})\.(\d{2})\.(\d{4})(.*)/);
 
         results = results.filter(path => {
-            console.log('path:', path);
             return dateRegularExpression.test(path)
         });
 
         results.forEach(path => {
-            let newName = path.replace(dateRegularExpression, "$1$4.$3.$2");
+            let newName = path.replace(dateRegularExpression, "$1$4.$3.$2$5");
+            
             fs.rename(path, newName, function (err) {
                 if (err) console.log('ERROR: ' + err);
             });
